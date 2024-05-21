d220eba4fa96dcf071d00089bb032bf0e6ba1fef
Brannon Dorsey <brannon@brannondorsey.com>
2024-05-21 12:04:34 -0400

    Add solutions up through options3

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,14 +14,20 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
+#[derive(Debug)]
 struct Team {
     goals_scored: u8,
     goals_conceded: u8,
+}
+
+fn default_team() -> Team {
+    Team {
+        goals_scored: 0,
+        goals_conceded: 0,
+    }
 }
 
 fn build_scores_table(results: String) -> HashMap<String, Team> {
@@ -39,6 +45,30 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        if !scores.contains_key(&team_1_name) {
+            scores.insert(team_1_name.clone(), default_team());
+        }
+        if !scores.contains_key(&team_2_name) {
+            scores.insert(team_2_name.clone(), default_team());
+        }
+
+        let current_values = scores.get(&team_1_name).unwrap();
+        scores.insert(
+            team_1_name.clone(),
+            Team {
+                goals_scored: current_values.goals_scored + team_1_score,
+                goals_conceded: current_values.goals_conceded + team_2_score,
+            },
+        );
+
+        let current_values = scores.get(&team_2_name).unwrap();
+        scores.insert(
+            team_2_name.clone(),
+            Team {
+                goals_scored: current_values.goals_scored + team_2_score,
+                goals_conceded: current_values.goals_conceded + team_1_score,
+            },
+        );
     }
     scores
 }
